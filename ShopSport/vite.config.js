7b2df485476c5dc96cbbb5a8f7e9f088da8897f0
lbc <gringo871@hotmail.com>
2023-07-28 01:29:20 -0300

    PreEntrega/zanotti

--- ShopSport/vite.config.js
+++ ShopSport/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https:zanotti871.github.io/vite-deploy/React-Proyecto1/'
+ base:'https:zanotti871.github.io/React-Proyecto1/'
 })
